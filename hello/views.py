f8e1742ece298d3ee0e3b92c0e2e9e67c8c6674d
Alan Jui <alanjui.1960@gmail.com>
2024-01-29 22:14:41 +0800

    將 hello.views.hello_there() 改用 Django Templates 回應。

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,4 @@
-import re
-
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.timezone import datetime
 
@@ -9,17 +8,7 @@ def home(request):
 
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y ad %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+    print(request.build_absolute_uri())
+    return render(
+        request, "hello/hello_there.html", {"name": name, "date": datetime.now()}
+    )
